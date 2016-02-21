ec0dfc980f5489b8faba6ac768d74e9376c03082
galdinorosas <dino.rosas@gmail.com>
2016-02-21 00:25:00 -0800

    final changes were made where the fill-in text is displayed after the user clicks submit. Also when the user clicks restart the game or mad libs is restarted.

--- angular.js
+++ angular.js
@@ -1,6 +1,51 @@
 angular.module('myApp',[])
 .controller('firstCtrl', function($scope){
-		$scope.userName = 'name';
+	$scope.madlibs = {
+		userName: 'name1',
+
+	}
+
+	$scope.userName = 'name2';
+	$scope.job_title = 'job title';
+	$scope.tedious_task='tedious task';
+	$scope.dirty_task='dirty task';
+	$scope.celebrity='celebrity';
+	$scope.useless_skill='useless skill';
+	$scope.adjective='adjective';
+	$scope.obnoxiuous_celebrity = 'obnoxiuous celebrity';
+	$scope.huge_number='a large number';
+	$scope.gender="gender (male or female)"
+	$scope.he="he";
+	$scope.she="she";
+	$scope.him="him";
+	$scope.her="her";
+	$scope.his="his";
+	$scope.heOrShe = "he /or she";
+	$scope.hisOrHer = "his /or her";
+	$scope.himOrHer = "him /or her";
+
+	$scope.test = function(f){
+		console.log(f);
+		if(f.$valid){
+			console.log('true');
+		}
+	}
+	$scope.textDisplay = true;
+
+	$scope.submit = function(){
+		if( $scope.myForm.$valid ) {
+		    console.log('The form is valid');
+		    $scope.textDisplay = false;
+		    console.log($scope.textDisplay);
+
+		} else {
+		    console.log('The form is invalid');
+		}
+	};
+
+	$scope.restart = function(){
+		$scope.textDisplay = true;
+		$scope.userName = 'name2';
 		$scope.job_title = 'job title';
 		$scope.tedious_task='tedious task';
 		$scope.dirty_task='dirty task';
@@ -8,7 +53,7 @@ angular.module('myApp',[])
 		$scope.useless_skill='useless skill';
 		$scope.adjective='adjective';
 		$scope.obnoxiuous_celebrity = 'obnoxiuous celebrity';
-		$scope.huge_number='huge number';
+		$scope.huge_number='a large number';
 		$scope.gender="gender (male or female)"
 		$scope.he="he";
 		$scope.she="she";
@@ -18,6 +63,11 @@ angular.module('myApp',[])
 		$scope.heOrShe = "he /or she";
 		$scope.hisOrHer = "his /or her";
 		$scope.himOrHer = "him /or her";
+
+	};
+
+	
+
 
 });
 
