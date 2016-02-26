a83145cab5e2ddb1d6cc449eae901dd992335b04
galdinorosas <dino.rosas@gmail.com>
2016-02-26 00:10:33 -0800

    Cleaned up code and added comments.

--- angular.js
+++ angular.js
@@ -1,93 +1,81 @@
-angular.module('myApp',['ngAnimate'])
-.controller('firstCtrl', function($scope,$timeout){
+// Angular module with depended ngAnimate module initialized.
+angular.module('myApp', ['ngAnimate'])
+    // Controller initialized with defining $scope and $timeout 
+    // services as function parameters.
+    .controller('firstCtrl', function($scope, $timeout) {
 
+        $scope.userName = "";
+        $scope.job_title = "";
+        $scope.tedious_task = "";
+        $scope.dirty_task = "";
+        $scope.celebrity = "";
+        $scope.useless_skill = "";
+        $scope.adjective = "";
+        $scope.obnoxiuous_celebrity = "";
+        $scope.huge_number = "";
+        $scope.gender = "";
+        $scope.he = "he";
+        $scope.she = "she";
+        $scope.him = "him";
+        $scope.her = "her";
+        $scope.his = "his";
+        $scope.textDisplay = true;
+        $scope.inputDisplay = true;
 
-	$scope.userName = "";
-	$scope.job_title = "";
-	$scope.tedious_task="";
-	$scope.dirty_task="";
-	$scope.celebrity="";
-	$scope.useless_skill="";
-	$scope.adjective="";
-	$scope.obnoxiuous_celebrity = "";
-	$scope.huge_number="";
-	$scope.gender="";
-	$scope.he="he";
-	$scope.she="she";
-	$scope.him="him";
-	$scope.her="her";
-	$scope.his="his";
+        // Submit function which hides the input display and shows the text display
+        // after a 1 second delay.
+        $scope.submit = function() {
+            if ($scope.myForm.$valid) {
+                console.log('The form is valid');
+                $scope.inputDisplay = false;
 
+                $timeout(function() {
+                    $scope.textDisplay = false;
+                }, 1000);
 
-	
+                console.log($scope.textDisplay);
 
-	$scope.test = function(f){
-		console.log(f);
-		if(f.$valid){
-			console.log('true');
-		}
-	}
-	$scope.textDisplay = true;
-	$scope.inputDisplay = true;
+            } else {
+                console.log('The form is invalid');
+            }
+        };
 
-	$scope.submit = function(){
-		if( $scope.myForm.$valid ) {
-		    console.log('The form is valid');
-		    $scope.inputDisplay = false;
+        // Restart function, sends the user to the initial input screen with
+        // previously user typed values wiped.
+        $scope.restart = function() {
+            $scope.textDisplay = true;
+            $timeout(function() {
+                $scope.inputDisplay = true;
+                $scope.userName = "";
+                $scope.job_title = "";
+                $scope.tedious_task = "";
+                $scope.dirty_task = "";
+                $scope.celebrity = "";
+                $scope.useless_skill = "";
+                $scope.adjective = "";
+                $scope.obnoxiuous_celebrity = "";
+                $scope.huge_number = "";
+                $scope.gender = "";
+                $scope.he = "he";
+                $scope.she = "she";
+                $scope.him = "him";
+                $scope.her = "her";
+                $scope.his = "his";
+            }, 1000);
 
-		    $timeout(function(){
-		    	$scope.textDisplay = false;
-		    }, 1000);
-		    
-		    console.log($scope.textDisplay);
+        };
 
-		} else {
-		    console.log('The form is invalid');
-		}
-	};
+        // Go back button that lets the user go back to the input screen
+        // with the previously input values still present.
+        $scope.goBack = function() {
+            $scope.textDisplay = true;
+            $timeout(function() {
 
-	$scope.restart = function(){
-		$scope.textDisplay = true;
-		$timeout(function() {
-			$scope.inputDisplay = true;
-			$scope.userName = "";
-			$scope.job_title = "";
-			$scope.tedious_task="";
-			$scope.dirty_task="";
-			$scope.celebrity="";
-			$scope.useless_skill="";
-			$scope.adjective="";
-			$scope.obnoxiuous_celebrity = "";
-			$scope.huge_number="";
-			$scope.gender="";
-			$scope.he="he";
-			$scope.she="she";
-			$scope.him="him";
-			$scope.her="her";
-			$scope.his="his";
-		}, 1000);
-		
-	};
+                $scope.inputDisplay = true;
+            }, 1000);
 
-	$scope.goBack = function(){
-		$scope.textDisplay = true;
-		$timeout(function() {
-			
-			$scope.inputDisplay = true;
-		}, 1000);
-
-	};
+        };
 
 
 
-});
-
-// ng-init="userName='male name'; 
-// job_title='job title';
-// tedious_task='tedious task'; 
-// dirty_task='dirty task'; 
-// celebrity='celebrity'; 
-// useless_skill='useless skill'; 
-// adjective='adjective'; 
-// obnoxiuous_celebrity = 'obnoxiuous celebrity'; 
-// huge_number='huge number'"
+    });
